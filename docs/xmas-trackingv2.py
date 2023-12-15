a5dbc8a81b83ba8465c94fefbc32e751361e8354
hfranco <145460275+hfrancomiranda@users.noreply.github.com>
2023-12-14 23:25:44 -0500

    Update xmas-trackingv2.py

--- docs/xmas-trackingv2.py
+++ docs/xmas-trackingv2.py
@@ -55,7 +55,7 @@ for person, purchases_to_date in my_dict.items():
         ]
     )
 
-    fig.write_image(f'{person}_gauge_chart.png')
+    fig.write_image(f'docs/assets/images/{person}_gauge_chart.png')
 
 
 
