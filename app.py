31cae0ff94901b034a1a11efab695c28dc386eee
hfranco <145460275+hfrancomiranda@users.noreply.github.com>
2023-12-26 19:52:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,29 @@
-import pandas as pd
-import warnings
-import plotly.graph_objects as go
 import dash
-from dash.dependencies import Input, Output
-import dash_bootstrap_components as dbc
-from dash import html
-import numpy as np
-import warnings
-from dash import dcc
-import plotly.graph_objs as go
-import plotly.subplots as sp
+from dash import dcc, html
 import plotly.express as px
-from dash import dash_table
-warnings.filterwarnings('ignore')
 
-# import file
-Location = r'docs/details_final.xlsx'
+# Sample data
+data = {
+    'X': [1, 2, 3, 4, 5],
+    'Y': [10, 12, 8, 15, 7]
+}
 
-# read file
-df = pd.read_excel(Location, sheet_name='details')
+# Create a Dash web application
+app = dash.Dash(__name__)
 
-family_purchases = df[['Name', 'Price']].loc[(df['Order By'] == 'Us'), :].groupby('Name').sum().reset_index()
-
-# Convert DataFrame to dictionary
-my_dict = dict(zip(family_purchases['Name'], family_purchases['Price']))
-
-total_budget = 375
-
-# Create a gauge chart for each person
-for person, purchases_to_date in my_dict.items():
-    # Calculate the percentage of purchases to date compared to the total budget
-    percentage_purchases = (purchases_to_date / total_budget) * 100
-    amount_left = total_budget - purchases_to_date
-
-    # Create a gauge chart
-    fig = go.Figure()
-
-    fig.add_trace(go.Indicator(
-        mode="gauge+number",
-        value=percentage_purchases,
-        title={'text': f"{person}'s Purchases vs Total Budget"},
-        domain={'x': [0, 1], 'y': [0, 1]}
-    ))
-
-    fig.update_layout(
-        template="plotly_dark",
-        annotations=[
-            {
-                "x": 0.5,
-                "y": 0.5,
-                "text": f"{person}'s Purchases: {purchases_to_date}<br>Total Budget: {total_budget}<br>Amount Left: {amount_left}<br>{percentage_purchases:.2f}%",
-                "showarrow": False,
-                "font": {'size': 15}  # Adjust the font size here
-            }
-        ]
+# Define the layout of the app
+app.layout = html.Div(children=[
+    html.H1("Dash Plotly Example"),
+    
+    # Scatter plot using Plotly Express
+    dcc.Graph(
+        id='scatter-plot',
+        figure=px.scatter(data, x='X', y='Y', title='Scatter Plot')
     )
+])
 
-    fig.write_image(f'docs/{person}_gauge_chart.png')
-    fig.show()
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
 
 
